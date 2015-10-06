c94785ddcd496580b8cdf978905e9c7006efaf3e
Akash Krishnan <ak@aakay.net>
2015-10-06 09:29:36 -0400

    impl login form; need to impl auth session/cookie

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ module.exports = function ( app, sockets ) {
   app.get( '/', index );
   app.get( '/config.json', config );
   app.get( '/register', register );
+  app.post( '/api/login', loginAccount );
   app.post( '/api/register', registerAccount );
 
   //require( './public.js' )( app, sockets );
@@ -45,6 +46,45 @@ function register( req, res, next ) {
 
 }
 
+function loginAccount( req, res ) {
+
+  // Ensure guest (i.e. no user)
+  if ( !req.user ) {
+    new multiparty.Form().parse( req, Utils.safeFn( function ( err, fields ) {
+      if ( err ) {
+        res.json( { err: err } );
+      } else {
+        try {
+
+          // Check if user exists with username and password
+          User.get(
+            {
+              username: fields.username[ 0 ],
+              password: fields.password[ 0 ]
+            },
+            Utils.safeFn( function ( err, user ) {
+              if ( err ) {
+                res.json( { err: err } );
+              } else {
+
+                // TODO: we need to sign in the user
+
+              }
+            } )
+          );
+
+        } catch ( err ) {
+          Logger.error( err );
+          res.json( { err: err.toString() } );
+        }
+      }
+    } ) );
+  } else {
+    res.status( 400 ).json( { err: 'Invalid request. Authenticated users may not register accounts.' } );
+  }
+
+}
+
 function registerAccount( req, res ) {
 
   // Ensure guest (i.e. no user)
@@ -60,12 +100,14 @@ function registerAccount( req, res ) {
               username: fields.username[ 0 ],
               password: fields.password[ 0 ]
             },
-            Utils.safeFn( function ( err ) {
+            Utils.safeFn( function ( err, user ) {
+              if ( err ) {
+                res.json( { err: err } );
+              } else {
 
-              // TODO: we need to sign in the user
+                // TODO: we need to sign in the user
 
-              res.json( { err: err } );
-
+              }
             } )
           );
         } catch ( err ) {
