01b83fe44bb6d32f576c74c3e3760e730426b545
Akash Krishnan <ak@aakay.net>
2015-10-14 11:49:48 -0400

    improved scaffolding

--- public/controllers/scaffoldController.js
+++ public/controllers/scaffoldController.js
@@ -2,18 +2,10 @@
 
 (function () {
 
-  document.querySelector( '[home]' ).addEventListener( 'click', function ( e ) {
-    window.location.replace( '/' );
-  } );
-
   document.querySelector( '#search' ).addEventListener( 'keydown', function ( e ) {
     if ( e.keyCode === 13 ) {
       window.location.replace( '/' + e.target.value );
     }
-  } );
-
-  document.querySelector( '[logout]' ).addEventListener( 'click', function ( e ) {
-    window.location.replace( '/logout' );
   } );
 
 })();
